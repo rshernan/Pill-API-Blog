ed37287849c81852b91887e583e1f91f263a43e9
rshernan <rsolerhernan@gmail.com>
2021-01-14 17:55:49 +0100

    commit for merge

--- assets/javascript/modal/modal.js
+++ assets/javascript/modal/modal.js
@@ -5,11 +5,11 @@ backgroundModal.append(modal);
 modal.append(closeButton);
 
 function inspectPost(userId, postId){
-    const postTitle = $("<h1 class='title_modal'>Post Title</h1>");
-    const postBody = $("<p></p>");
-    const userTitle = $("<h2 class='title_modal'>USER</h2>");
-    const userName = $("<p></p>");
-    const userEmail = $("<p></p>");
+    const postTitle = $("<h1 class='title__modal'>Post Title</h1>");
+    const postBody = $("<p>Example</p>");
+    const userTitle = $("<h2 class='title__modal'>USER</h2>");
+    const userName = $("<p>Example</p>");
+    const userEmail = $("<p>Example</p>");
     const titleComments = $("<h1 class='title__modal'>Comments</h1>");
     const buttonComments = $("<button class='button__modal__load'>Load Comments</button>");
     buttonComments.on("click",function(){
@@ -24,10 +24,17 @@ function inspectPost(userId, postId){
         }
     );
     $('body').append(backgroundModal);
+    modal.append(postTitle);
+    modal.append(postBody);
+    modal.append(userTitle);
+    modal.append(userName);
+    modal.append(userEmail);
+    modal.append(titleComments);
+    modal.append(buttonComments);
 }
 
 function editPost(){
-    const postTitle = $("<h1 class='title_modal'>Edit post</h1>");
+    const postTitle = $("<h1 class='title__modal'>Edit post</h1>");
     const labelTitle = $("<label>Title</label>");
     const labelBody = $("<label>Body</label>");
     const inputTitle = $("<input/>");
