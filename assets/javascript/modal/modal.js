2a3d420f13a19e06c1454cebce85bee66fda5e56
iarrieta90 <irati_arrieta90@hotmail.com>
2021-01-15 10:45:04 +0100

    delete ok

--- assets/javascript/modal/modal.js
+++ assets/javascript/modal/modal.js
@@ -4,39 +4,38 @@ const closeButton = $('<button class="button__modal__close">close</button>');
 backgroundModal.append(modal);
 modal.append(closeButton);
 
-function inspectPost(userId, postId){
-    const postTitle = $("<h1 class='title_modal'>Post Title</h1>");
-    const postBody = $("<p>Example</p>");
-    const userTitle = $("<h2 class='title_modal'>USER</h2>");
-    const userName = $("<p>Example</p>");
-    const userEmail = $("<p>Example</p>");
-    const titleComments = $("<h1 class='title__modal'>Comments</h1>");
-    const buttonComments = $("<button class='button__modal__load'>Load Comments</button>");
-    buttonComments.on("click",function(){
-        loadComents(postId);
-    })
-    $.ajax("https://jsonplaceholder.typicode.com/users/"+userId)
-    .then(
-        function success(data){
-            console.log(data);
-        }, function failed(error){
-
-        }
-    );
-    $('body').append(backgroundModal);
-    modal.append(postTitle);
-    modal.append(postBody);
-    modal.append(userTitle);
-    modal.append(userName);
-    modal.append(userEmail);
-    modal.append(titleComments);
-    modal.append(buttonComments);
+function inspectPost(userId, postId) {
+  const postTitle = $("<h1 class='title_modal'>Post Title</h1>");
+  const postBody = $("<p>Example</p>");
+  const userTitle = $("<h2 class='title_modal'>USER</h2>");
+  const userName = $("<p>Example</p>");
+  const userEmail = $("<p>Example</p>");
+  const titleComments = $("<h1 class='title__modal'>Comments</h1>");
+  const buttonComments = $(
+    "<button class='button__modal__load'>Load Comments</button>"
+  );
+  buttonComments.on("click", function () {
+    loadComents(postId);
+  });
+  $.ajax("https://jsonplaceholder.typicode.com/users/" + userId).then(
+    function success(data) {
+      console.log(data);
+    },
+    function failed(error) {}
+  );
+  $("body").append(backgroundModal);
+  modal.append(postTitle);
+  modal.append(postBody);
+  modal.append(userTitle);
+  modal.append(userName);
+  modal.append(userEmail);
+  modal.append(titleComments);
+  modal.append(buttonComments);
 }
 
-function editPost(postId){
-    console.log(postId);
+function editPost(postId) {
 
-    const editModal = $(`
+  const editModal = $(`
     <div class="modal_container">
         <div class="toast_container"></div>
         <div class="modal">
@@ -54,63 +53,76 @@ function editPost(postId){
                 </form>
             </div>
         </div>
-    </div>`
-    )
+    </div>`);
 
+  $("#modal").append(editModal);
 
-    $("#modal").append(editModal);
-    
+  $("#edit_form").on("submit", function (e) {
+    e.preventDefault();
 
-    $("#edit_form").on("submit", function(e){
+    var requestStatus = $(`<h3 class="status"></h3>`);
+    requestStatus.text("Submitting...");
 
-        e.preventDefault();
+    $(".toast_container").append("<div class='toast'></div>");
+    $(".toast").append(requestStatus);
 
-        var requestStatus = $(`<h3 class="status"></h3>`);
-        requestStatus.text("Submitting...");
-
-        $(".toast_container").append("<div class='toast'></div>")
-        $(".toast").append(requestStatus)
-
-        $.ajax(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
-            method: "PUT",
-            contentType: "application/json",
-            data: JSON.stringify({
-                title: $("#post_title").val(),
-                body: $("#post_body").val(),
-            })
-        
-        }).then(
-            function success(data) {
-                var submittedData1 = $(`<p>Title: ${data.title}</p>`);
-                var submittedData2 = $(`<p>Body: ${data.body}</p>`)
-                requestStatus.text(`Successful request!!`);
-                $(".toast").append(submittedData1);
-                $(".toast").append(submittedData2);
-                
-            },
-            function failed(err){
-                requestStatus.text("Request failed");
-            }
-        )
-    })
-}
-
-function deletePost(postId){
+    setTimeout(function(){
+        $(".toast").fadeOut(1500);
+    },1500)
 
     $.ajax(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
-        method: "DELETE",
-        contentType: "application/json",
-    })
+      method: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify({
+        title: $("#post_title").val(),
+        body: $("#post_body").val(),
+      }),
+    }).then(
+      function success(data) {
+        var submittedData1 = $(`<p>Title: ${data.title}</p>`);
+        var submittedData2 = $(`<p>Body: ${data.body}</p>`);
+        requestStatus.text(`Successful request!!`);
+        $(".toast").append(submittedData1);
+        $(".toast").append(submittedData2);
+      },
+      function failed(err) {
+        requestStatus.text("Request failed");
+      }
+    );
+  });
 }
 
-function loadComents(postId){
-    $.ajax("https://jsonplaceholder.typicode.com/posts/"+postId+"/comments")
-    .then(
-        function success(data){
-            console.log(data);
-        }, function failed(error){
+function deletePost(postId) {
+  var requestStatus = $(`<h3 class="status"></h3>`);
+  requestStatus.text("Deleting...");
 
-        }
-    );
+    setTimeout(function(){
+        $(".toast").fadeOut(1500);
+    },1000)
 
+  $("body").append("<div class='toast'></div>");
+  $(".toast").append(requestStatus);
+
+  $.ajax(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+    method: "DELETE",
+    contentType: "application/json",
+  }).then(
+    function success(data) {
+        requestStatus.text(`Post deleted!!`);
+    },
+    function failed(err) {
+        requestStatus.text("Request failed");
+    }
+  );
+}
+
+function loadComents(postId) {
+  $.ajax(
+    "https://jsonplaceholder.typicode.com/posts/" + postId + "/comments"
+  ).then(
+    function success(data) {
+      console.log(data);
+    },
+    function failed(error) {}
+  );
 }
